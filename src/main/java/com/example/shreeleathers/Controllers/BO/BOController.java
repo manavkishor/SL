c575bc1d3678b454d56bcba1d4001ee634afc3fa
manavkishor <manavkishor@gmail.com>
2025-01-09 12:20:07 +0530

    Back Office layout created

--- src/main/java/com/example/shreeleathers/Controllers/BO/BOController.java
+++ src/main/java/com/example/shreeleathers/Controllers/BO/BOController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class PurchaseController implements Initializable {
+public class BOController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
--- src/main/java/com/example/shreeleathers/Controllers/BO/BOMenuController.java
+++ src/main/java/com/example/shreeleathers/Controllers/BO/BOMenuController.java
@@ -1,4 +1,13 @@
 package com.example.shreeleathers.Controllers.BO;
 
-public class BOMenuController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BOMenuController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
