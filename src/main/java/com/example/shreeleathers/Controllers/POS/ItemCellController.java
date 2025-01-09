28b94c94cc9b8f99c83e206c4635492949263c2f
manavkishor <manavkishor@gmail.com>
2025-01-09 13:21:51 +0530

    Item cell design created

---
+++ src/main/java/com/example/shreeleathers/Controllers/POS/ItemCellController.java
@@ -0,0 +1,25 @@
+package com.example.shreeleathers.Controllers.POS;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ItemCellController implements Initializable {
+    public Text item_Code_lbl;
+    public Text item_Name_lbl;
+    public Text size_lbl;
+    public Text quantity_lbl;
+    public Text rate_lbl;
+    public Text salesman_lbl;
+    public Button remove_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
