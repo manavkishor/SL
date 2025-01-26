700d444b0111b012159e5ba800a56061f6281e9e
Manav <manavkishor@gmail.com>
2025-01-26 12:57:33 +0530

    message box code updated

--- src/main/java/com/example/shreeleathers/Controllers/MessageBoxController.java
+++ src/main/java/com/example/shreeleathers/Controllers/MessageBoxController.java
@@ -37,12 +37,28 @@ public class MessageBoxController implements Initializable
     public void giveError(String message)
     {
         message_lbl.setText(message);
-        msg_typ_icon.setGlyphName("ERROR");
+        msg_typ_lbl.setText("ERROR!");
+        msg_typ_icon.setGlyphName("TIMES_CIRCLE_ALT");
+        msg_typ_icon.setGlyphStyle("-fx-fill : red");
+        ok_btn.setVisible(false);
+        Stage stage = (Stage) message_lbl.getScene().getWindow();
+        close_btn.setOnAction(e -> stage.close());
     }
 
     public void askQuestion(String message)
     {
         message_lbl.setText(message);
-        msg_typ_icon.setGlyphName("QUESTION");
+        msg_typ_lbl.setText("QUESTION?");
+        msg_typ_icon.setGlyphName("QUESTION_CIRCLE");
+        Stage stage = (Stage) message_lbl.getScene().getWindow();
+        close_btn.setOnAction(e -> stage.close());
+        ok_btn.setOnAction(event -> doAction());
+    }
+
+    private void doAction()
+    {
+        System.out.println("Data set to database");
+        Stage stage = (Stage) message_lbl.getScene().getWindow();
+        stage.close();
     }
 }
